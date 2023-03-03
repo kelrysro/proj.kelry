1338b271793f80f0b8be7be88ddf13d8dad8a252
kelrysro <kelryrodrigues86@gmail.com>
2023-03-03 16:33:15 -0300

    termineeeeeiii

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,34 @@
 <script >
-export default {
-  data(){
-    return{
-      contador:0
+   export default {
+  data() {
+    return {
+      contador: 0
     }
   },
   methods: {
-    encrementadorContador(){
+    incrementarContador() {
       this.contador++
     },
     decrementarContador() {
       this.contador--
-    },
-    
+    }
+  },
+  mounted() {
+    console.log(`O valor inicial do contador é ${this.contador}.`)
   }
 }
 
 </script>
 
 <template>
-    <button @click="incrementarContador">Incrementar</button>
-    <button @click="decrementarContador">Decrementar</button>
-    <p>Valor do contador é: {{ contador }}</p>
+  <button @click="incrementarContador">Incrementar</button>
+  <button @click="decrementarContador">Decrementar</button>
+  <p>Valor do contador é: {{ contador }}</p>
 </template>
 
-<style scoped >
-  button {
-    font-weight: bold;
-  }
+<style scoped>
+button {
+  font-weight: bold;
+}
 </style>
+
